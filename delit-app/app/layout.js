d6796651d6d27ccfa62a3b9e24ba15092c83560b
ram <rameswarreddy78@gmail.com>
2024-09-22 11:18:56 +0530

    Navbar updated

--- delit-app/app/layout.js
+++ delit-app/app/layout.js
@@ -20,8 +20,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100;300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-white antialiased font-josefin `}
       >
         {children}
       </body>
