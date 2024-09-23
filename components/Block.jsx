204d9b86c6e1d4dde31a8a655d3be9cabac8b334
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-09-23 23:12:53 +0530

    feat: add favicon and og image

--- components/Block.jsx
+++ components/Block.jsx
@@ -9,8 +9,10 @@ export default function Block() {
           <span className="absolute max-md:-bottom-6 text-5xl md:top-[calc(14%)] md:left-3/4 lg:text-8xl md:text-5xl tracking-widest bg-gradient-to-br from-black to-colorC bg-clip-text text-transparent">MAGAZINES </span>
       </div>
       <div className="md:w-3/6 mb-7 mt-[20%] text-center md:text-justify">
-          <p className="lg:text-lg md:text-sm">We call ourselves 'dreamers and doers' for a reason: we can make happen not just what is possible, but what is impossible. NEOM is a unique investment opportunity, unrivalled anywhere else. This is not business as usual. Be a part of it. Invest in the new future now, invest in NEOM.</p>
-          <Link href="/magazines"><button className="bg-gradient-to-br from-colorA to-colorB hover:from-black hover:to-colorB text-white lg:py-4 lg:px-6 md:py-2 md:px-3 lg:text-lg md:text-sm rounded-full mt-10 px-3 py-2">EXPLORE NOW !</button></Link>
+          <p className="lg:text-lg md:text-sm">We call ourselves 'dreamers and doers' for a reason: we can make happen not just what is possible, but what is impossible. NEOM is a unique investment opportunity, unrivalled anywhere else.</p>
+          <Link href="/magazines">
+            <button className="bg-gradient-to-br from-colorA to-colorB hover:from-black hover:to-colorB text-white lg:py-4 lg:px-6 md:py-2 md:px-3 lg:text-lg md:text-sm rounded-full mt-10 px-3 py-2">EXPLORE NOW !</button>
+          </Link>
       </div>
     </div>
   )
