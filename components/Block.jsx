6be4583f52c5bb03aeeb77f38db64e06fd7c12e0
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-09-26 19:40:58 +0530

    feat: add transitions to sticky scroll effect for home page blocks

--- components/Block.jsx
+++ components/Block.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
 import Link from 'next/link';
+import clsx from 'clsx';
 
-export default function Block({title, image, isActive}) {
+export default function Block({title, image, description, isActive}) {
+  const titleClasses = {
+    "md:left-3/4 md:top-[calc(14%)]": isActive,
+    "md:left-[169%] md:top-0": !isActive
+  }
+  const blockClasses = {
+    "opacity-1": isActive,
+    "opacity-0": !isActive
+  }
+  const contentClasses = {
+    "translate-y-0": isActive,
+    "translate-y-20": !isActive
+  }
+
+
   return (
-    <div className={`min-h-[100vh] flex items-center justify-center sticky top-0 ${isActive ? "visible" : "hidden"}`}>
-      <div className="flex md:flex-row lg:space-x-24 m-20 items-center flex-col md:space-x-10 max-md:space-y-12 self-stretch max-h-1/3">
-        <div className="relative self-stretch md:w-1/3 max-md:flex max-md:flex-col max-md:items-center max-md:text-center">
-            <img className="w-2/3 md:object-cover md:w-full md:h-full" src={image} alt="" />
-            <span className="absolute max-md:-bottom-6 text-5xl md:top-[calc(14%)] md:left-3/4 lg:text-8xl md:text-5xl tracking-widest bg-gradient-to-br from-black to-colorC bg-clip-text text-transparent">{title}</span>
+    <div className={clsx("min-h-[85vh] flex items-center justify-center sticky top-[15vh] overflow-x-hidden transition-opacity duration-700", blockClasses)}>
+      <div className="flex md:flex-row lg:space-x-24 my-auto mx-20 items-center flex-col md:space-x-10 max-md:space-y-12 self-stretch max-h-1/3">
+        <div className="relative self-stretch md:w-1/3 max-md:flex max-md:flex-col max-md:items-center max-md:text-center max-md:h-[40vh]">
+            <img className="object-cover w-full h-full" src={image} alt="" />
+            <span className={clsx("absolute max-md:-bottom-6 text-5xl lg:text-8xl md:text-5xl tracking-widest bg-gradient-to-br from-black to-colorC bg-clip-text text-transparent transition-all duration-700", titleClasses)}>{title}</span>
         </div>
-        <div className="md:w-3/6 mb-7 mt-[20%] text-center md:text-justify">
-            <p className="lg:text-lg md:text-sm">We call ourselves 'dreamers and doers' for a reason: we can make happen not just what is possible, but what is impossible. NEOM is a unique investment opportunity, unrivalled anywhere else.</p>
+        <div className="md:w-3/6 mt-[20%] mb-7 text-center md:text-justify">
+            <p className={clsx("lg:text-lg md:text-sm max-md:text-xs transition-all duration-700", contentClasses)}>{description}</p>
             <Link href="/magazines">
-              <button className="bg-gradient-to-br from-colorA to-colorB hover:from-black hover:to-colorB text-white lg:py-4 lg:px-6 md:py-2 md:px-3 lg:text-lg md:text-sm rounded-full mt-10 px-3 py-2">EXPLORE NOW !</button>
+              <button className="transition-none bg-gradient-to-br from-colorA to-colorB hover:from-black hover:to-colorB text-white lg:py-4 lg:px-6 md:py-2 md:px-3 lg:text-lg md:text-sm rounded-full mt-10 px-3 py-2">EXPLORE NOW !</button>
             </Link>
         </div>
       </div>
--- components/BlockSection.jsx
+++ components/BlockSection.jsx
@@ -7,35 +7,37 @@ const data = [
 {
     "id": 0,
     "image": "/mag.jpg",
-    "title": "MAGAZINES"
+    "title": "MAGAZINES",
+    "description": "We call ourselves 'dreamers and doers' for a reason: we can make happen not just what is possible, but what is impossible. NEOM is a unique investment opportunity, unrivalled anywhere else."
 },
 {
     "id": 1,
-    "image": "/mag.jpg",
-    "title": "BLOGS"
+    "image": "https://images.unsplash.com/photo-1528359645462-5ff224bf906e?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "title": "BLOGS",
+    "description": "Pandas are large, peaceful bears native to the mountainous forests of China, known for their distinctive black-and-white fur. They primarily eat bamboo, which makes up over 99% of their diet, though they occasionally eat other plants and small animals. Pandas are solitary animals and spend most of their time eating and resting."
 },
 {
     "id": 2,
-    "image": "https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "title": "GALLERY"
+    "image": "https://plus.unsplash.com/premium_photo-1664303314018-d59cbbb5b13d?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "title": "GALLERY",
+    "description": "I am ChatGPT, an AI language model created by OpenAI, designed to assist with a wide range of tasks, including answering questions, generating text, solving problems, and even engaging in creative writing"
 },
 {
     "id": 3,
     "image": "https://plus.unsplash.com/premium_photo-1661844581764-eb881c5a7462?q=80&w=1428&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "title": "CLUB\u00A0TALK"
-}
-]
+    "title": "CLUB\u00A0TALK",
+    "description": "Thanos is basically the biggest boss in the Marvel Universe, no cap. Dude literally rolled up with the Infinity Gauntlet, snapped his fingers, and wiped out half the universe like it was nothing. All because he thought overpopulation was a vibe killer. He’s mad powerful, pulling off stuff no one else could, but like, super chill about it."
+}];
 
 export default function BlockSection() {
     const targetRef = useRef(null);
     const [activeBlock, setActiveBlock] = useState(0);
     const {scrollYProgress} = useScroll({target: targetRef, offset: ["start start", "end end"]});
     const activeBlockValue = useTransform(scrollYProgress, (latest) => Math.floor(latest * 4));
-    useEffect(() => activeBlockValue.onChange(latest => {setActiveBlock(latest)}), [])
+    useEffect(() => activeBlockValue.on("change", latest => {setActiveBlock(latest)}), [])
     return (
       <>
-      <motion.p className="fixed top-5 left-5 text-lg">{activeBlock}</motion.p>
-        <motion.div ref={targetRef} className={`min-h-[${data.length + 1}00vh]`}>
+        <motion.div ref={targetRef} className={`min-h-[${(data.length + 1) * 100}vh]`}>
         {data.map(block => <Block key={block.id} {...block} isActive={activeBlock === block.id} />)}
         </motion.div>
       </>
