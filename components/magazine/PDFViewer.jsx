006f78c3f9eb3355c814660856e04377ad5efede
saijayanth59 <sithlord5995@gmail.com>
2024-09-27 08:32:52 +0530

    build: deployment test regarding react-pdf dependency

--- components/magazine/PDFViewer.jsx
+++ components/magazine/PDFViewer.jsx
@@ -6,10 +6,13 @@ import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   "pdfjs-dist/legacy/build/pdf.worker.min.mjs",
+//   import.meta.url
+// ).toString();
+
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
 
 const options = {
   cMapUrl: "/cmaps/",
