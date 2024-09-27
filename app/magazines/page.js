18b4f3a01c23096f951eb0772c99e8cf72ee55bb
saijayanth59 <sithlord5995@gmail.com>
2024-09-27 08:22:47 +0530

    fix: solve polyfill promises for dependency(react-pdf)

--- app/magazines/page.js
+++ app/magazines/page.js
@@ -1,7 +1,9 @@
-import MagazineWrapper from '@/components/magazine/MagazineWrapper'
+import "core-js/full/promise/with-resolvers.js";
+import polyfillPromiseWithResolvers from "@/utils/polyfilsResolver";
+import MagazineWrapper from "@/components/magazine/MagazineWrapper";
+
+polyfillPromiseWithResolvers();
 
 export default function page() {
-  return (
-    <MagazineWrapper />
-  )
+  return <MagazineWrapper />;
 }
