eb31c904284241ca524c89128e6ea17fa61fc2ff
G S S Mannar <mannargottumukkala2020@gmail.com>
2024-09-28 15:56:41 +0530

    updates banner

--- app/page.js
+++ app/page.js
@@ -2,12 +2,13 @@ import Footer from "@/components/footer/Footer";
 import Navbar  from "@/components/Navbar/Navbar";
 import BlockSection from "@/components/BlockSection";
 import Banner from "@/components/Banner";
-
+import Test from "@/components/Test";
 export default function Home() {
   return (
     <>
     <Navbar />
     <Banner />
+    
     <BlockSection />
     <Footer />
     </>
