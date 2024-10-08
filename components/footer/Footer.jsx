773b1d680861ac8bcbea7c959f05e65e2c41ba57
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-10-08 15:48:37 +0530

    style(footer): newsletter field, contact us

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -2,6 +2,7 @@
 // components/Footer.js
 "use client";
 import React, { useState } from "react";
+import { FaWhatsapp, FaInstagram, FaLinkedin, FaRegEnvelope } from "react-icons/fa";
 
 const Footer = () => {
   const [email, setEmail] = useState("");
@@ -18,10 +19,10 @@ const Footer = () => {
       <div className="container mx-auto   px-4 py-0">
         <div className=" flex flex-col lg:flex-row justify-between items-center mb-8">
           {/* Logo Section */}
-          <div className=" mb-6 lg:mb-0">
+          <div className=" mb-6 lg:mb-0 flex-grow">
             {/* <h1 className="text-2xl font-bold text-white">Your Logo</h1> */}
             {/* <img src="/footer_logo.jpg" alt="" className="w-32  mx-auto h-auto"/> */}
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.0" className="w-32  mx-auto h-auto" viewBox="0 0 504.000000 495.000000" preserveAspectRatio="xMidYMid meet">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.0" className="w-64  mx-auto h-auto" viewBox="0 0 504.000000 495.000000" preserveAspectRatio="xMidYMid meet">
 
             <g transform="translate(0.000000,495.000000) scale(0.100000,-0.100000)" fill="currentColor" stroke="none">
             <path d="M4400 3483 c-201 -20 -561 -97 -1135 -240 -77 -20 -165 -41 -196 -48 -61 -13 -79 -21 -79 -36 0 -19 74 -8 292 45 300 72 406 96 417 96 5 0 12 -12 15 -27 17 -81 37 -547 43 -991 6 -503 6 -503 27 -500 19 3 21 11 27 118 18 323 2 1091 -26 1304 -7 56 -10 105 -5 109 21 23 441 105 680 133 107 13 150 22 150 31 0 10 -22 13 -87 11 -49 0 -104 -3 -123 -5z"/>
@@ -37,7 +38,7 @@ const Footer = () => {
           </div>
 
           {/* Uploading Buttons */}
-          <div className="flex flex-col  justify-center items-center mb-8">
+          <div className="flex flex-col flex-grow justify-center items-center mb-8">
           <h2 className="text-xl font-semibold text-white mb-4">
             Contribute
           </h2>
@@ -57,57 +58,18 @@ const Footer = () => {
               </button>
             </div>
           </div>
-          
-          {/* Social Links */}
-          <div className="flex flex-col  justify-center items-center mb-8 mt-8">
-          <h2 className="text-xl font-semibold text-white mb-4">
-            Follow Us On
-          </h2>
-          <div className="flex justify-center space-x-4 mb-6">
-          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
-            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M17.472 14.382c-.297-.149-1.758-.867-2.031-.967-.272-.099-.469-.149-.666.149-.198.297-.763.966-.935 1.164-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.134.297-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.372-.025-.521-.075-.149-.666-1.611-.915-2.209-.24-.577-.485-.499-.666-.499h-.572c-.198 0-.521.074-.795.372-.272.297-1.04 1.017-1.04 2.479 0 1.461 1.065 2.875 1.213 3.074.149.198 2.1 3.212 5.077 4.506.709.306 1.261.489 1.692.626.711.226 1.357.194 1.868.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.413-.074-.124-.272-.198-.57-.347z" />
-              <path d="M20.52 3.477C18.253 1.209 15.243 0 12 0 5.373 0 0 5.373 0 12c0 2.122.557 4.205 1.617 6.037L0 24l6.064-1.589A11.949 11.949 0 0 0 12 24c6.627 0 12-5.373 12-12 0-3.243-1.209-6.253-3.477-8.523zM12 22.129c-1.835 0-3.651-.491-5.22-1.419l-.375-.224-3.602.943.963-3.509-.244-.364C2.42 15.862 1.872 13.952 1.872 12 1.872 6.601 6.601 1.872 12 1.872c2.536 0 4.921.988 6.707 2.773 1.785 1.786 2.773 4.171 2.773 6.707 0 5.399-4.729 10.257-10.257 10.257z" />
-            </svg>
-          </a>
-
-          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
-            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M19 0h-14c-2.75 0-5 2.25-5 5v14c0 2.75 2.25 5 5 5h14c2.75 0 5-2.25 5-5v-14c0-2.75-2.25-5-5-5zm-11.75 20.5h-3v-11h3v11zm-1.5-12.63c-.97 0-1.75-.79-1.75-1.75s.79-1.75 1.75-1.75c.97 0 1.75.79 1.75 1.75s-.78 1.75-1.75 1.75zm13.25 12.63h-3v-5.57c0-1.33-.03-3.04-1.85-3.04-1.86 0-2.14 1.45-2.14 2.95v5.66h-3v-11h2.88v1.5h.04c.4-.75 1.39-1.54 2.86-1.54 3.06 0 3.62 2.01 3.62 4.63v6.41z" />
-            </svg>
-          </a>
-
-          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
-            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M12 2.163c3.204 0 3.584.012 4.849.07 1.366.062 2.633.343 3.608 1.317.975.975 1.256 2.242 1.318 3.608.058 1.265.069 1.645.069 4.849s-.012 3.584-.07 4.849c-.062 1.366-.343 2.633-1.318 3.608-.975.975-2.242 1.256-3.608 1.318-1.265.058-1.645.069-4.849.069s-3.584-.012-4.849-.07c-1.366-.062-2.633-.343-3.608-1.318-.975-.975-1.256-2.242-1.318-3.608-.058-1.265-.069-1.645-.069-4.849s.012-3.584.07-4.849c.062-1.366.343-2.633 1.318-3.608.975-.975 2.242-1.256 3.608-1.318 1.265-.058 1.645-.069 4.849-.069m0-2.163c-3.257 0-3.667.013-4.947.072-1.39.064-2.645.343-3.688 1.386-1.043 1.043-1.322 2.297-1.386 3.688-.059 1.28-.072 1.69-.072 4.947s.013 3.667.072 4.947c.064 1.39.343 2.645 1.386 3.688 1.043 1.043 2.297 1.322 3.688 1.386 1.28.059 1.69.072 4.947.072s3.667-.013 4.947-.072c1.39-.064 2.645-.343 3.688-1.386 1.043-1.043 1.322-2.297 1.386-3.688.059-1.28.072-1.69.072-4.947s-.013-3.667-.072-4.947c-.064-1.39-.343-2.645-1.386-3.688-1.043-1.043-2.297-1.322-3.688-1.386-1.28-.059-1.69-.072-4.947-.072z" />
-              <path d="M12 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zm0 10.162a3.999 3.999 0 1 1 0-7.998 3.999 3.999 0 0 1 0 7.998z" />
-              <circle cx="18.406" cy="5.594" r="1.44" />
-            </svg>
-          </a>
-        </div>
-        <h2 className="text-xl font-semibold text-white mb-4">
-            Contact Us
-          </h2>
-          <div className="flex justify-center space-x-4 mb-6">
-          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
-          <svg  className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M20 3H4C2.9 3 2 3.9 2 5v14c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 2v.51l-8 5-8-5V5h16zM4 19v-9.99l7.5 4.68c.31.2.69.2 1 0L20 9.01V19H4z" />
-          </svg>
-          </a>
-          
-        </div>
-          </div>
 
           {/* Newsletter Subscription */}
-        <div className="flex flex-col items-start justify-start mb-8">
+        <div className="flex flex-col flex-grow items-center justify-center mb-8">
           <h2 className="text-xl font-semibold text-white mb-4">
             Subscribe to our Newsletter
           </h2>
+          <p className="text-white mb-7 max-w-96 text-center">Stay Updated with the latest events, publications and oppurtunites from De-Lit!!!</p>
           <form onSubmit={handleSubscribe} className="w-full max-w-md">
-            <div className="flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-y-0 sm:space-x-2">
+            <div className="flex flex-row justify-center items-center p-2 focus-within:ring-2 rounded-full focus-within:ring-blue-500 bg-white">
               <input
                 type="email"
-                className="w-full px-4 py-2 text-gray-900 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full px-4 py-2 text-gray-900 rounded-full focus:outline-none flex-grow-[2] border-none"
                 placeholder="Enter your email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
@@ -115,43 +77,34 @@ const Footer = () => {
               />
               <button
                 type="submit"
-                className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700 transition-colors duration-300 w-full sm:w-auto"
+                className="bg-black px-6 py-3 rounded-full hover:bg-gradient-to-r hover:from-black hover:to-colorB hover:transition-all duration-500 text-sm tracking-widest"
               >
-                Subscribe
+                SUBSCRIBE
               </button>
             </div>
           </form>
         </div>
-        
-
-          {/* Navigation Links */}
-          {/* <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 text-center">
-            <a href="#" className="hover:text-white">
-              Home
-            </a>
-            <a href="#" className="hover:text-white">
-              Magazines
-            </a>
-            <a href="#" className="hover:text-white">
-              Blogs
-            </a>
-            <a href="#" className="hover:text-white">
-              Gallery
-            </a>
-            <a href="#" className="hover:text-white">
-              About Us
-            </a>
-            <a href="#" className="hover:text-white">
-              Contact
-            </a>
-          </div> */}
         </div>
 
-        
-
-        
-
-        
+         <div className="flex flex-col  justify-center items-center mb-8 mt-8">
+          <h2 className="text-xl font-semibold text-white mb-4">
+            Contact Us
+          </h2>
+          <div className="flex justify-center space-x-4 mb-6">
+          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
+            <FaWhatsapp size={50}/>
+          </a>
+          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
+            <FaInstagram size={50} />
+          </a>
+          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
+            <FaLinkedin size={50} />
+          </a>
+          <a href="#" className="hover:text-white transition-transform duration-300 hover:scale-125">
+            <FaRegEnvelope size={50} />
+          </a>
+        </div>
+          </div>
 
         {/* Copyright Section */}
         <div className="text-center text-sm text-gray-500">
@@ -160,6 +113,6 @@ const Footer = () => {
       </div>
     </footer>
   );
-};
+}
 
 export default Footer;
