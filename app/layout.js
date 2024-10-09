8fe7c47484b15599ed1a61857c1c5e179d1d5e4e
saijayanth59 <sithlord5995@gmail.com>
2024-10-09 14:09:27 +0530

    fix: merge conflict

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,6 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar/Navbar";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Footer from "@/components/Footer";
-=======
-import Footer from "@/components/footer/Footer";
->>>>>>> 927fde7 (style(publications): complete layout for publications page)
-=======
-import Footer from "@/components/footer/Footer";
-<<<<<<< HEAD
->>>>>>> 927fde7 (style(publications): complete layout for publications page)
-=======
->>>>>>> ed0cd0e777cb9c5e537b149046f766d87e5c67fd
->>>>>>> 0ec2928dbab15ac9ca3e9b2da072c99d5259595b
 
 export const metadata = {
   title: "De-LiT",
