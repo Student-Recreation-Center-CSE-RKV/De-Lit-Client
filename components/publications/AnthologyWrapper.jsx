ba77b984eaf4929f8e448429fadf04b810b15165
saijayanth59 <sithlord5995@gmail.com>
2024-10-09 13:35:13 +0530

    chore: change components structure(wrapper)

---
+++ components/publications/AnthologyWrapper.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CardWrapper from "./CardWrapper";
+import { ANTHOLOGIES } from "@/utils/dummy";
+
+export default function AnthologyWrapper() {
+  return (
+    <CardWrapper
+      data={ANTHOLOGIES}
+      header={"A Journey Through Words: Our Anthologies"}
+      texPos={"md:text-center"}
+    />
+  );
+}
--- components/publications/Modal.jsx
+++ components/publications/Modal.jsx
@@ -1,43 +1,33 @@
 "use client";
 import { useRef, useEffect } from "react";
 import PDFViewer from "@/components/publications/PDFViewer";
-export default function Modal({ open, handleClose }) {
+export default function Modal({ open, handleClose, data }) {
   const dialog = useRef();
   useEffect(() => {
     if (open) {
-      document.getElementById("body").classList.toggle("blur-sm");
-      // document.getElementById('body').classList.toggle("opacity-70")
       dialog.current.showModal();
     } else {
-      document.getElementById("body").classList.toggle("blur-sm");
-      // document.getElementById('body').classList.toggle("opacity-70")
       dialog.current.close();
     }
-  }, [open]);
-
+  }, []);
   return (
     <dialog
       ref={dialog}
       onClose={handleClose}
-      className="z-20 md:w-4/5 m-auto mb-0 md:mt-[5%] rounded-t-xl overflow-scroll no-scrollbar"
+      className="z-20 md:w-4/5 m-auto w-full mb-0 md:mt-[7.5%] rounded-t-xl overflow-scroll no-scrollbar md:mx-auto"
     >
       <button
-        className="fixed z-30 m-2 w-8 h-8 md:m-5 rounded-full border-2  hover:border-colorA"
+        className="fixed z-30 m-2 w-8 h-8 md:m-5"
         onClick={handleClose}
       >
         X
       </button>
 
-      <h1 className="text-4xl md:text-7xl text-center mt-5 p-5">
-        A journey to death
+      <h1 className="text-3xl md:text-7xl text-center mt-5 p-5">
+        {data.title}
       </h1>
+      {/* <p className="mx-24 mb-12 text-2xl text-colorB">{data.text}</p> */}
       <PDFViewer file={"./sample.pdf"} />
-      {/* <embed
-          src="sample.pdf#toolbar=0"
-          width="100%"
-          height="400"
-          type="application/pdf"
-        /> */}
     </dialog>
   );
 }
