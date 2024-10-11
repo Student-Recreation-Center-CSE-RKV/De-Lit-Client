cbdcdbca2b9d3eca568a3f67acade2e8d546902c
saijayanth59 <sithlord5995@gmail.com>
2024-10-11 08:45:50 +0530

    fix(publications): improve mobile responsiveness by adding meta tags

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="user-scalable=no, width=device-width, initial-scale=1.0"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
       <body id="body">
         <Navbar />
         {children}
