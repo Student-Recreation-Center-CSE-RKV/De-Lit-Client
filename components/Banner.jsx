5fab43b0d3b81043f39f7bdc2c9e78f466996c12
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-10-11 15:53:26 +0530

    style(banner): remove logo from banner, fix Link component disturbance for blocks

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -11,23 +11,12 @@ const Banner = () => {
       style={{ backgroundImage: `url(${BANNER_IMAGE})` }}
     >
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="absolute inset-0 bg-black opacity-50 -z-0"></div>
       {/* Content */}
-      <div className="relative h-full flex md:flex-row flex-col items-center md:justify-around justify-center">
-        <div className="md:w-1/2 max-md:h-1/2">
-          <img
-            className="max-md:object-contain max-md:w-full max-md:h-full md:w-3/4 opacity-70 m-auto"
-            src="/stroke.svg"
-            alt="Magazine"
-          />
-        </div>
-        <div className="text-white md:w-1/2">
-          <h1
-            className={`text-[2.5rem] leading-[1.2] md:text-[5rem] w-2/3 max-md:text-center max-md:m-auto ${font.className}`}
-          >
+      <div className="relative h-screen md:w-2/3 w-5/6 m-auto flex items-center justify-center">
+          <h1 className={`text-white md:leading-[1] md:text-[5rem] text-3xl text-center ${font.className}`} >
             Building a like-minded community!
           </h1>
-        </div>
       </div>
     </div>
   );
