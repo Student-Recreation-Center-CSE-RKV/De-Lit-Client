af8b9cd26a888ace004fd765d9b9e9f296840f27
santhikumar148 <santhikumaryadavalli23@gmail.com>
2024-10-13 13:13:07 +0530

    style(clubtalk): adjust size and margin for heading

--- app/clubtalk/page.js
+++ app/clubtalk/page.js
@@ -2,10 +2,10 @@ import Testimonials from "@/components/clubtalk/Testimonials";
 
 export default function Page() {
     return (
-        <div className="container mt-[25vh]">
-            <div class="max-md:mb-20">
-                <h1 className="md:text-8xl text-6xl text-center tracking-wide">CLUB TALK</h1>
-                <p className="text-center mt-5">See what's our club members are talking</p>
+        <div className="container mt-[20vh]">
+            <div class="max-md:mb-20 max-md:-mt-10 -mb-20">
+                <h1 className="md:text-[6rem] text-[2.5rem] text-center tracking-wide">CLUB TALK</h1>
+                <p className="text-center">See what's our club members are talking</p>
             </div>
             <Testimonials />
         </div>
