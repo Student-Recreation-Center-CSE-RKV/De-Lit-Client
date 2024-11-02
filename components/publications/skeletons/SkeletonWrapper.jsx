d11f10abe87e1445ad82bb993959650fe3373805
saijayanth59 <sithlord5995@gmail.com>
2024-11-02 13:23:46 +0530

    fix(publications): remove fallbacks in page.js and add lazying loading for every wrapper section along with it's fallback

--- components/publications/skeletons/SkeletonWrapper.jsx
+++ components/publications/skeletons/SkeletonWrapper.jsx
@@ -6,7 +6,7 @@ export default function SkeletonWrapper({ Skeleton }) {
       <div className="ml-3 md:ml-16 mb-9">
         <HeaderSkeleton />
         <div className="w-full overflow-x-auto gap-3 p-4 flex no-scrollbar scroll-smooth snap-mandatory snap-x">
-          {Array.from({ length: 4 }).map((_, index) => (
+          {Array.from({ length: 5 }).map((_, index) => (
             <Skeleton key={index} />
           ))}
         </div>
