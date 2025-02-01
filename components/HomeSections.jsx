87d6af07a10ebf9afbb597e0c6758f282c277092
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-02-01 18:02:04 +0530

    refactor(homepage): use 'use-gesture' package to detect scroll events

--- components/HomeSections.jsx
+++ components/HomeSections.jsx
@@ -5,73 +5,74 @@ import BlockSection from "./BlockSection";
 import { useRouter } from "next/navigation";
 import Footer from "./Footer";
 import { useSwipeable } from "react-swipeable";
+import { useGesture, useWheel } from "@use-gesture/react";
 
 export default function HomeSections({ blocksData, bannerData }) {
   const router = useRouter();
+  const [canScroll, setCanScroll] = useState(true);
+  const [currentBlock, setCurrentBlock] = useState(0);
 
   useEffect(() => {
     router.push("#banner");
+    setCurrentBlock(0);
   }, []);
 
   blocksData.forEach((block) => {
     block.block_title = block.block_title.toUpperCase();
-    block.id = block.block_title.toLowerCase().replace(/[^a-z]/g, "");  // keep only alphabets
+    block.id = block.block_title.toLowerCase().replace(/[^a-z]/g, ""); // keep only alphabets
   });
 
-  const ids = ["banner"].concat(blocksData.map((block) => block.id)).concat(["footer"]);
-  const [currentBlock, setCurrentBlock] = useState(0);
-  
+  const ids = ["banner"]
+    .concat(blocksData.map((block) => block.id))
+    .concat(["footer"]);
+
   // for mobile swipe
   const swipeHandlers = useSwipeable({
-    onSwipeStart: ({dir}) => {
-      if (dir === "Up") scrollDown(() => {});
-      else if (dir === "Down") scrollUp(() => {});
-    }
+    onSwipeStart: ({ dir }) => {
+      if (dir === "Up") scrollDown();
+      else if (dir === "Down") scrollUp();
+    },
   });
 
   // the function to scroll down
-  const scrollDown = (removeListener) => {
+  const scrollDown = () => {
     if (currentBlock === ids.length - 1) return;
-    router.push("#" + ids[currentBlock + 1]);
+    router.replace("#" + ids[currentBlock + 1]);
     setCurrentBlock(currentBlock + 1);
-    removeListener();
-  }
+    setCanScroll(false);
+  };
 
   // the function to scroll up
-  const scrollUp = (removeListener) => {
+  const scrollUp = () => {
     if (currentBlock === 0) return;
-    router.push("#" + ids[currentBlock - 1]);
-    setCurrentBlock(currentBlock - 1); 
-    removeListener();
-  }
+    router.replace("#" + ids[currentBlock - 1]);
+    setCurrentBlock(currentBlock - 1);
+    setCanScroll(false);
+  };
+
+  const bind = useWheel(state => {
+      if (canScroll) {
+        if (state.direction[1] > 0) scrollDown();
+        else if (state.direction[1] < 0) scrollUp();
+      }
+  });
 
   useEffect(() => {
-    const removeWheelListener = () => window.removeEventListener("wheel", handleScroll);
-    const removeKeyListener = () => window.removeEventListener("keydown", handleKeyPress);
-
-    const handleScroll = (event) => {
-      if (event.deltaY > 0) scrollDown(removeWheelListener);
-      else scrollUp(removeWheelListener);
-    }
-
+    const delay = setTimeout(() => setCanScroll(true), 900);
     const handleKeyPress = (event) => {
-      if (event.key === "ArrowDown") scrollDown(removeKeyListener);
-      else if (event.key === "ArrowUp") scrollUp(removeKeyListener);
-    }
-
-    const delay = setTimeout(() => {
-        window.addEventListener("wheel", handleScroll);
-        window.addEventListener("keydown", handleKeyPress);
-    }, 1000);
-
-    return () => {
-      window.removeEventListener("wheel", handleScroll);
+      if (!canScroll) return;
+      if (event.key === "ArrowDown") scrollDown();
+      else if (event.key === "ArrowUp") scrollUp();
+    };
+    window.addEventListener("keydown", handleKeyPress);
+    return () => { 
       clearTimeout(delay);
-    }
-  }, [currentBlock]);
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [canScroll]);
 
   return (
-    <div {...swipeHandlers}>
+    <div {...swipeHandlers} {...bind()}>
       <Banner bannerData={bannerData} />
       <BlockSection blocksData={blocksData} />
       <Footer />
