8165fc8075cf090548af620bf7448ab7cc2e4ecb
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-02-05 19:46:52 +0530

    style(aboutus): set a min-height for the modal

--- components/aboutus/Modal.jsx
+++ components/aboutus/Modal.jsx
@@ -6,7 +6,7 @@ export default function Modal({ member, setShowModal }) {
       <div class="fixed inset-0 bg-black opacity-50 z-[55]"></div>
       <div className="fixed inset-0 z-[57] overflow-auto">
         <div className="flex justify-center items-center h-screen">
-          <div className="flex flex-col bg-white rounded-2xl md:w-[50%] w-[90%]">
+          <div className="flex flex-col bg-white rounded-2xl md:w-[50%] w-[90%] min-h-[50%]">
             <div className="sticky top-0 p-4 self-end z-[60]">
               <button
                 className="bg-black p-1 md:p-2 rounded-full text-mywhite hover:text-white"
@@ -15,11 +15,13 @@ export default function Modal({ member, setShowModal }) {
                 <FaTimes size={17} />
               </button>
             </div>
-            <div className="p-10">
-              <h1 className="text-3xl font-bold text-center -mt-10">
+            <div className="p-10 flex-1 flex flex-col items-center justify-center">
+              <h1 className="text-3xl font-bold text-center -mt-10 justify-self-start">
                 {member.member_name}
               </h1>
-              <p className="text-lg mt-10 text-center">{member.about}</p>
+              <div className="flex items-center justify-center mt-5">
+                <p className="text-lg text-center">{member.about}</p>
+              </div>
             </div>
           </div>
         </div>
