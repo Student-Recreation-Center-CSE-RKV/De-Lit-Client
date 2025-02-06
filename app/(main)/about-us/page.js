be2ecee33cbfcf297837f70b4f87ca4f8294b792
Santhi Kumar Yadavalli <santhikumaryadavalli23@gmail.com>
2025-02-06 16:54:44 +0530

    feat(aboutus): get info-sections data from api

--- app/(main)/about-us/page.js
+++ app/(main)/about-us/page.js
@@ -16,18 +16,12 @@ export default async function Page() {
     batchMap[member.batch].members.push(member);
   });
 
+  const infoSections = (await getData("get_all_infos")).infos;
   return (
     <div className="container mt-[20vh]">
-      <ContentBlock 
-        title="The Story of De-Lit" 
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-      />
-      <ContentBlock 
-        title="Our Vision" 
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-      />
-      <h1 className="text-center text-5xl mt-10">The Fellowship</h1>
-      <p className="text-center text-lg mt-5">Meet the team that makes it all happen</p>
+      {infoSections.map((infoSection) => (
+        <ContentBlock key={infoSection.id} title={infoSection.title} description={infoSection.content} />
+      ))}
       <AllBatches allBatches={batchMap}/>
     </div>
   )
